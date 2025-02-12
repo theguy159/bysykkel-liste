7d20ed457ff8fad10186b5871ade067128779418
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2025-02-12 17:17:15 +0100

    feat: Use mock data to display a simple list of stations that are installed and renting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    CLIENT_IDENTIFIER: 'Mathias-bysykkel_list',
+    API_URL: 'https://gbfs.urbansharing.com/oslobysykkel.no/station_status.json',
+    REFRESH_FREQUENCY_SECONDS: '10'
+  },
 };
 
 export default nextConfig;
